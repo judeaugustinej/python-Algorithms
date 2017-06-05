626efbaf92020380a05ee4e48273dc0071f85ee7
Jude Augustine Job <judeaugustinej@gmail.com>
2017-06-05 09:00:40 +0530

    Learning LinkList.
    * created the base folder structure
    * Added the code for singly link-list
    
    TODO:
    * Use properties for getters and setters
    * Add support for add and delete (before head, after tail, any position)

---
+++ linkedList/README.md
@@ -0,0 +1,9 @@
+* What is linked List
+```
+```
+* Advantages of Linked List
+* Singly Linked List
+* Doubly Linked List
+* Circular Linked List
+* Application
+* Problems
---
+++ linkedList/singlyLinkList.py
@@ -0,0 +1,64 @@
+class Node(object):
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+    def get_data(self):
+        return self.data
+
+    def set_data(self, data):
+        self.data = data
+
+    def get_next(self):
+        return self.next
+
+    def set_next(self, next):
+        self.next = next
+
+
+class LinkedList(object):
+    def __init__(self, r=None):
+        self.root = r
+        self.size = 0
+
+    def get_size(self):
+        return self.size
+
+    def add(self, d):
+        new_node = Node(d, self.root)
+        self.root = new_node
+        self.size +=1
+
+    def remove(self, d):
+        this_node = self.root
+        prev_node = None
+        while this_node:
+            if this_node.get_data() == d:
+                if prev_node:
+                    prev_node.set_next(this_node.get_next())
+                else:
+                    self.root = this_node
+                self.size -= 1
+                return True #data removed
+            else:
+                prev_node = this_node
+                this_node = this_node.get_next()
+        return False
+
+    def find(self, d):
+        this_node = self.root
+        while this_node:
+            if this_node.get_data() == d:
+                return d
+            else:
+                this_node = this_node.get_next()
+        return None
+
+if __name__ == "__main__":
+    myList = LinkedList()
+    myList.add(6)
+    myList.add(8)
+    myList.add(11)
+    print(myList)
+    print(myList.find(11))
+    print(myList.find(111))
